6b20c592c6f0e3847509ec89291847b061207514
c-mabe <74473554+c-mabe@users.noreply.github.com>
2021-08-02 16:01:57 -0400

    Add mobile optimization and animations

---
+++ script-who-we-are.js
@@ -0,0 +1,20 @@
+//Fade in animation
+function showText(id) {
+    document.getElementById(id).style.display = "inline";
+  }
+  
+  const observer = new IntersectionObserver(entries => {
+      // Loop over the entries
+      entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add('fadein');
+          entry.target.classList.add('visible');
+          
+        }
+      });
+    });
+    
+    observer.observe(document.querySelector('#fadein1'));
+    observer.observe(document.querySelector('#fadein2'));
